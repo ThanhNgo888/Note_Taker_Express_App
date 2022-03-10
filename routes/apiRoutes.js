dbed741bc6c76daaa351f649523345cb3cfde476
Thanh Ngo <thanhngo26@hotmail.com>
2022-03-10 11:12:49 -0800

    routes modified

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,31 +1,101 @@
-const router = require("express").Router();
-const store = require("../db/store");
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+const fs = require("fs");
 
+const generateUniqueId = require("generate-unique-id");
 
+const editNote = (updatedNotesArray) => {
+  fs.writeFile("./db/db.json", JSON.stringify(updatedNotesArray), (err) => {
+    if (err) throw err;
+  });
+};
 
-// module.exports = function(app) {} => check it
+// ROUTING
+module.exports = (app) => {
+  // GET REQUEST
+  // Setup the /api/notes GET route
+  app.get("/api/notes", (req, res) => {
+    // Read the db.json file and return all saved notes as JSON.
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) throw err;
+      // Parse the JSON string into a JavaScript object
+      res.json(JSON.parse(data));
+    });
+  });
 
-// GET "/api/notes" responds with all notes from the database
-router.get("/api/notes", function(req, res) {
-  store
-    .getNotes()
-    .then(notes => res.json(notes))
-    .catch(err => res.status(500).json(err));
-});
+  // POST REQUEST
+  // Setup the /api/notes post route
+  app.post("/api/notes", (req, res) => {
+    // Receives a new note, adds it to the db.json file, returns the new note to the client
+    const newNote = req.body;
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) throw err;
+      // Parse the JSON string into a JavaScript object
+      const notesArr = JSON.parse(data);
+      newNote.id = generateUniqueId({ length: 10 });
+      notesArr.push(newNote);
 
-router.post("/api/notes", (req, res) => {
-  store
-    .addNote(req.body)
-    .then((note) => res.json(note))
-    .catch(err => res.status(500).json(err));
-});
+      editNote(notesArr);
+      console.log(
+        `New Note Added! Title: ${JSON.stringify(
+          newNote.title
+        )}, Text: ${JSON.stringify(newNote.text)}, ID: ${newNote.id}`
+      );
 
-// DELETE "/api/notes" deletes the note with an id equal to req.params.id
-router.delete("/api/notes/:id", function(req, res) {
-  store
-    .removeNote(req.params.id)
-    .then(() => res.json({ ok: true }))
-    .catch(err => res.status(500).json(err));
-});
+      res.send(notesArr);
+    });
+  });
 
-module.exports = router;
+  // DELETE REQUEST
+  // Setup the /api/notes/:id delete route
+  app.delete("/api/notes/:id", (req, res) => {
+    const deleteId = req.params.id;
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) throw err;
+      let notesArr = JSON.parse(data);
+      // removes the note with the given id property
+      for (let i = 0; i < notesArr.length; i++) {
+        if (notesArr[i].id === deleteId) {
+          notesArr.splice(i, 1);
+        }
+      }
+      editNote(notesArr);
+      console.log(`Note Deleted! Note ID: ${deleteId}`);
+      res.send(notesArr);
+    });
+  });
+
+  // PUT REQUEST
+  // Setup the /api/notes/:id put route
+  app.put("/api/notes/:id", (req, res) => {
+    const editId = req.params.id;
+
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) throw err;
+
+      let notesArr = JSON.parse(data);
+
+      let selectedNote = notesArr.find((note) => note.id === editId);
+
+      // check if found
+      if (selectedNote) {
+        let updatedNote = {
+          title: req.body.title, // set value of `title` get from req
+          text: req.body.text, // set value of `text` get from req
+          id: selectedNote.id,
+        };
+        //  find index at which the item is stored in the array
+        let targetIndex = notesArr.indexOf(selectedNote);
+
+        //  replace object data with `updatedNote` object
+        notesArr.splice(targetIndex, 1, updatedNote);
+
+        res.sendStatus(204);
+        editNote(notesArr);
+        res.json(notesArr);
+      } else {
+        res.sendStatus(404);
+      }
+    });
+  });
+};
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,15 +1,22 @@
-var path = require("path");
-var router = require("express").Router();
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+const path = require("path");
 
-// All other routes respond with the index.html file
-router.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
+// ROUTING
+// exporting the router to other modules
+module.exports = (app) => {
+  // HTML GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases the user is shown an HTML page of content
 
-// "/notes" responds with the notes.html file
-router.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "../public/notes.html"));
-});
+  // GET /notes should return the notes.html file.
+  app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
 
-
-module.exports = router;
+  // GET * should return the index.html file.
+  // If no matching route is found default to home
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+};
